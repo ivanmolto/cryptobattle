b781a91205c4244b6790c8212a1ab9f50c9a99ef
Ivan Molto <ivanmolto@gmail.com>
2024-01-22 16:24:10 +0100

    chore: Add Instructions

---
+++ src/components/Instructions.jsx
@@ -0,0 +1,16 @@
+const Instructions = () => {
+  return (
+    <section className="mx-auto max-w-7xl overflow-hidden px-4 sm:px-6 lg:px-8">
+      <div className="border border-1 border-blue-100 bg-white px-4 pr-4 pl-4 pt-2 pb-2 sm:px-6 rounded-lg">
+        <div className="font-medium text-lg">Instructions</div>
+        <ol className="list-decimal list-inside text-gray-600">
+          <li>Enter an account hash for each player and submit</li>
+          <li>Battle</li>
+          <li>Check the winner</li>
+        </ol>
+      </div>
+    </section>
+  );
+};
+
+export default Instructions;
