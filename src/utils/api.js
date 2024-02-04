d666479e3cf4ab3cebd4093f86ec0599bde3eee0
Ivan Molto <ivanmolto@gmail.com>
2024-02-04 18:20:18 +0100

    chore: Add APIs

---
+++ src/utils/api.js
@@ -0,0 +1,140 @@
+function calculateScore(transactions, domains, favorites, subdomains) {
+  return transactions + domains + favorites + subdomains;
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => b.score - a.score);
+}
+
+function getAddress(address) {
+  return address;
+}
+
+async function getName(address) {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      ApiKey: import.meta.env.VITE_SOLANAFM_API_KEY,
+    },
+  };
+  try {
+    const response = await fetch(
+      `https://api.solana.fm/v0/accounts/${address}`,
+      options,
+    );
+    const display = await response.json();
+    var name = display.result.data.friendlyName;
+    return name;
+  } catch (err) {
+    return console.error(err);
+  }
+}
+
+async function getTransactions(address) {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      ApiKey: import.meta.env.VITE_SOLANAFM_API_KEY,
+    },
+  };
+  try {
+    const response = await fetch(
+      `https://api.solana.fm/v0/accounts/${address}/transactions?page=1`,
+      options,
+    );
+    const display = await response.json();
+    var transactions = display.result.data.length;
+    return transactions;
+  } catch (err) {
+    return console.error(err);
+  }
+}
+
+async function getDomains(address) {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      ApiKey: import.meta.env.VITE_SOLANAFM_API_KEY,
+    },
+  };
+  try {
+    const response = await fetch(
+      `https://api.solana.fm/v0/domains/bonfida/${address}`,
+      options,
+    );
+    const display = await response.json();
+    console.log(display.result);
+    var domains = display.result.length;
+    return domains;
+  } catch (err) {
+    return console.error(err);
+  }
+}
+
+async function getFavorites(address) {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      ApiKey: import.meta.env.VITE_SOLANAFM_API_KEY,
+    },
+  };
+  try {
+    const response = await fetch(
+      `https://api.solana.fm/v0/domains/bonfida/favourites/${address}`,
+      options,
+    );
+    const display = await response.json();
+    var favorites = display.result.domains.length;
+    return favorites;
+  } catch (err) {
+    return console.error(err);
+  }
+}
+
+async function getSubdomains(address) {
+  const options = { method: "GET", headers: { accept: "application/json" } };
+  try {
+    const response = await fetch(
+      `https://api.solana.fm/v0/domains/bonfida/subdomains/${address}`,
+      options,
+    );
+    const display = await response.json();
+    var subdomains = display.result.subdomains.length;
+    return subdomains;
+  } catch (err) {
+    return console.error(err);
+  }
+}
+
+function getUserData(player) {
+  return Promise.all([
+    getAddress(player),
+    getName(player),
+    getTransactions(player),
+    getDomains(player),
+    getFavorites(player),
+    getSubdomains(player),
+  ]).then(([address, name, transactions, domains, favorites, subdomains]) => ({
+    instance: {
+      address,
+      name,
+      transactions,
+      domains,
+      favorites,
+      subdomains,
+    },
+    score: calculateScore(transactions, domains, favorites, subdomains),
+  }));
+}
+
+export async function battle(players) {
+  const contender = await Promise.all([
+    getUserData(players[0]),
+    getUserData(players[1]),
+  ]);
+  return sortPlayers(contender);
+}
