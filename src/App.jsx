a8f3446b8d41217ec88edcc1e378407738ddd110
Ivan Molto <ivanmolto@gmail.com>
2024-02-04 18:25:27 +0100

    chore: Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Footer from "./components/Footer";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Loading from "./components/Loading";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const Results = React.lazy(() => import("./components/Results"));
+const Home = React.lazy(() => import("./components/Home"));
+const Battle = React.lazy(() => import("./components/Battle"));
+const NoMatch = React.lazy(() => import("./components/NoMatch"));
+
+const queryClient = new QueryClient();
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <div className="flex-col justify-between">
+          <Nav />
+          <React.Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/battle" element={<Battle />} />
+              <Route path="/results" element={<Results />} />
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </React.Suspense>
+          <Footer />
+        </div>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
