d23f24a9cbb63dcecb65dc5ec028caa2180e7dbe
Ivan Molto <ivanmolto@gmail.com>
2025-02-23 17:30:55 +0100

    feat: Update colors in Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,24 +8,24 @@ const Home = () => {
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
               <span className="block xl:inline font-montserrat">
-                Solana APIs
+                Avalanche APIs
               </span>{" "}
-              <span className="block text-blue-600 xl:inline font-montserrat">
+              <span className="block text-[#ff394a] xl:inline font-montserrat">
                 are Fun
               </span>
             </h1>
             <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl font-montserrat">
-              A casual game for Solana nerds.{" "}
+              A casual game for Avalanche nerds.{" "}
               <span className="hidden block:sm">
                 No need to connect any wallet!
               </span>
-              <br /> Are you ready for a battle against any hash account?
+              <br /> Are you ready for a battle against any wallet?
             </p>
             <div className="mx-auto mt-5 max-w-md sm:flex sm:justify-center md:mt-8">
               <div className="rounded-md drop-shadow-md">
                 <Link
                   to="/battle"
-                  className="flex w-full items-center justify-center rounded-md border border-1 bg-blue-600 px-8 py-3 text-base font-medium text-white hover:bg-blue-700 md:py-4 md:px-10 md:text-lg"
+                  className="flex w-full items-center justify-center rounded-md border border-1 bg-[ff394a] px-8 py-3 text-base font-medium text-white hover:bg-red-700 md:py-4 md:px-10 md:text-lg"
                 >
                   Get started
                 </Link>
